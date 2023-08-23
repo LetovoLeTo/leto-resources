ef930c6332baf280fda409de6073af925b60ec24
Milk-Cool <milan-pl@mail.ru>
2023-08-23 17:10:43 +0300

    added warning and error boxes

---
+++ server.js
@@ -0,0 +1,14 @@
+console.warn("THIS SCRIPT IS ONLY MADE FOR DEVELOPMENT PURPOSES");
+console.warn("DO NOT USE IN PRODUCTION");
+
+const express = require("express");
+const request = require("request");
+
+const app = express();
+const port = 3001;
+
+app.use("/res", express.static("."));
+app.all("*", async (req, res) => {
+    request("http://localhost:3000" + req.url).pipe(res);
+})
+app.listen(port);
