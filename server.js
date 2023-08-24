de8d451af826ca599a8accdf15aacac32062918c
Milk-Cool <milan-pl@mail.ru>
2023-08-24 19:36:29 +0300

    updated input and textare to have a max width of 90% of the page width

--- server.js
+++ server.js
@@ -7,8 +7,18 @@ const request = require("request");
 const app = express();
 const port = 3001;
 
+// app.use(express.raw({ "type": () => true }));
+
 app.use("/res", express.static("."));
 app.all("*", async (req, res) => {
-    request[req.method.toLowerCase()]("http://localhost:3000" + req.url).pipe(res);
+    const method = req.method.toLowerCase();
+    /*const options =  {
+        "url": "http://localhost:3000" + req.url,
+        "method": method.toUpperCase()
+    }
+    if(["patch", "post", "put"].includes(method))
+        options.body = req.body;
+    request(options).pipe(res);*/
+    req.pipe(request[method]("http://localhost:3000" + req.url)).pipe(res);
 })
 app.listen(port);
